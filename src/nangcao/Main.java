53bd9db99620c067f7476506a8f70f9578a9220d
3K3ki <danghung090@gmail.com>
2023-03-14 17:22:12 +0700

    commit

--- src/nangcao/Main.java
+++ src/nangcao/Main.java
@@ -6,8 +6,6 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-
-
         int choice = 0;
         while (choice != 6) {
             System.out.println("***************STUDENT MANAGE***************************");
@@ -25,14 +23,28 @@ public class Main {
 
                     break;
                 case 2:
+                    System.out.println("Nhập student ID:");
+                    Scanner scanner = new Scanner(System.in);
+                    int studentId = scanner.nextInt();
 
+                    scanner.nextLine();
+                    System.out.println("Nhập student name:");
+                    String studentName = scanner.nextLine();
+
+                    System.out.println("Nhập student age:");
+                    int age = scanner.nextInt();
+
+                    Student student = new Student(studentId, studentName, age);
+
+                    System.out.println("Student Thêm thành công!");
 
                     break;
                 case 3:
 
-
                     break;
                 case 4:
+                    System.out.println("Nhập id student để xóa:");
+                    int studentDel = input.nextInt();
 
                     break;
                 case 5:
